5d3f121d5e89ac6bd57dacd74677fa6dc7da4ffa
senrithfernando <rihansenrithvta@gmail.com>
2026-04-05 14:42:49 +0530

    chnages in reveversasio front page

--- frontend/src/pages/ReservationPage.jsx
+++ frontend/src/pages/ReservationPage.jsx
@@ -140,7 +140,7 @@ export function ReservationPage() {
                                 reservationTime.setHours(hours, minutes, 0, 0);
                                 
                                 const diffMins = (now - reservationTime) / (1000 * 60);
-                                if (diffMins >= 30) {
+                                if (diffMins >= 10) {
                                     shouldDelete = true;
                                 }
                             }
